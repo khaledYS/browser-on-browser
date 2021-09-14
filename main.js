e027b3426a161906903c8bd22b37a9a9a53b109c
AmmarYasser <admin@ammar.awash>
2021-09-14 13:48:48 +0300

    fixed an issue on redircting to no url page.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const iframe = document.querySelector('iframe')
 const url = location.href.split('?url=')
 if(url.length < 2 || url[1].trim() == ""){
-    window.open("/nourl!", "_self")
+    window.open("https://github.com/khaledYS/browser-on-browser/nourl!", "_self")
 }else{
     iframe.src = url[1]
 }
